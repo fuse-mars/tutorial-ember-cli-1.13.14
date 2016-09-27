be35c0a7154e989901ccedaad64fc5581102dfec
Marcellin Nshimiyimana <mars@fusemachines.com>
2016-09-27 11:04:15 -0400

    deploy dev version

---
+++ assets/app-test.js
@@ -0,0 +1,281 @@
+"use strict";
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+define('app-test/app', ['exports', 'ember', 'ember/resolver', 'ember/load-initializers', 'app-test/config/environment'], function (exports, _ember, _emberResolver, _emberLoadInitializers, _appTestConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _appTestConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _appTestConfigEnvironment['default'].podModulePrefix,
+    Resolver: _emberResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _appTestConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('app-test/components/app-version', ['exports', 'ember-cli-app-version/components/app-version', 'app-test/config/environment'], function (exports, _emberCliAppVersionComponentsAppVersion, _appTestConfigEnvironment) {
+
+  var name = _appTestConfigEnvironment['default'].APP.name;
+  var version = _appTestConfigEnvironment['default'].APP.version;
+
+  exports['default'] = _emberCliAppVersionComponentsAppVersion['default'].extend({
+    version: version,
+    name: name
+  });
+});
+define('app-test/controllers/array', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('app-test/controllers/object', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('app-test/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'app-test/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _appTestConfigEnvironment) {
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(_appTestConfigEnvironment['default'].APP.name, _appTestConfigEnvironment['default'].APP.version)
+  };
+});
+define('app-test/initializers/export-application-global', ['exports', 'ember', 'app-test/config/environment'], function (exports, _ember, _appTestConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_appTestConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var value = _appTestConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_appTestConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!window[globalName]) {
+        window[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete window[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('app-test/router', ['exports', 'ember', 'app-test/config/environment'], function (exports, _ember, _appTestConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _appTestConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {
+    this.route('search');
+  });
+
+  exports['default'] = Router;
+});
+define('app-test/routes/search', ['exports', 'ember'], function (exports, _ember) {
+	exports['default'] = _ember['default'].Route.extend({
+		actions: {
+			toggleMessage: function toggleMessage(type) {
+				this.controller.set('showMessage', type === 'OPEN');
+			}
+		}
+	});
+});
+define("app-test/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "revision": "Ember@1.13.11",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 2,
+            "column": 0
+          }
+        },
+        "moduleName": "app-test/templates/application.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(1);
+        morphs[0] = dom.createMorphAt(fragment, 0, 0, contextualElement);
+        dom.insertBoundary(fragment, 0);
+        return morphs;
+      },
+      statements: [["content", "outlet", ["loc", [null, [1, 0], [1, 10]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+define("app-test/templates/search", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    var child0 = (function () {
+      return {
+        meta: {
+          "revision": "Ember@1.13.11",
+          "loc": {
+            "source": null,
+            "start": {
+              "line": 7,
+              "column": 1
+            },
+            "end": {
+              "line": 11,
+              "column": 1
+            }
+          },
+          "moduleName": "app-test/templates/search.hbs"
+        },
+        arity: 0,
+        cachedFragment: null,
+        hasRendered: false,
+        buildFragment: function buildFragment(dom) {
+          var el0 = dom.createDocumentFragment();
+          var el1 = dom.createTextNode("		");
+          dom.appendChild(el0, el1);
+          var el1 = dom.createElement("span");
+          dom.setAttribute(el1, "class", "popover-content");
+          var el2 = dom.createTextNode("\n			Enter a partial or complete name\n		");
+          dom.appendChild(el1, el2);
+          dom.appendChild(el0, el1);
+          var el1 = dom.createTextNode("\n");
+          dom.appendChild(el0, el1);
+          return el0;
+        },
+        buildRenderNodes: function buildRenderNodes() {
+          return [];
+        },
+        statements: [],
+        locals: [],
+        templates: []
+      };
+    })();
+    return {
+      meta: {
+        "revision": "Ember@1.13.11",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 15,
+            "column": 6
+          }
+        },
+        "moduleName": "app-test/templates/search.hbs"
+      },
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("div");
+        dom.setAttribute(el1, "class", "full-name");
+        var el2 = dom.createTextNode("\n	Name\n	");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("button");
+        dom.setAttribute(el2, "class", "info-icon");
+        var el3 = dom.createTextNode("?");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createComment("");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("	");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("br");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n	");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("br");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n	");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("input");
+        dom.setAttribute(el2, "type", "text");
+        dom.setAttribute(el2, "name", "full-name");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var element0 = dom.childAt(fragment, [0]);
+        var element1 = dom.childAt(element0, [1]);
+        var morphs = new Array(3);
+        morphs[0] = dom.createElementMorph(element1);
+        morphs[1] = dom.createElementMorph(element1);
+        morphs[2] = dom.createMorphAt(element0, 3, 3);
+        return morphs;
+      },
+      statements: [["element", "action", ["toggleMessage", "OPEN"], ["on", "mouseEnter"], ["loc", [null, [4, 2], [4, 51]]]], ["element", "action", ["toggleMessage", "CLOSE"], ["on", "mouseLeave"], ["loc", [null, [5, 2], [5, 52]]]], ["block", "if", [["get", "showMessage", ["loc", [null, [7, 7], [7, 18]]]]], [], 0, null, ["loc", [null, [7, 1], [11, 8]]]]],
+      locals: [],
+      templates: [child0]
+    };
+  })());
+});
+/* jshint ignore:start */
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('app-test/config/environment', ['ember'], function(Ember) {
+  var prefix = 'app-test';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = Ember['default'].$('meta[name="' + metaName + '"]').attr('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  return { 'default': config };
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+if (!runningTests) {
+  require("app-test/app")["default"].create({"name":"app-test","version":"0.0.0+19faab0b"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=app-test.map
---
+++ assets/tests.js
@@ -0,0 +1,209 @@
+define('app-test/tests/acceptance/show-searchinput-tooltip-on-hover-test', ['exports', 'qunit', 'app-test/tests/helpers/module-for-acceptance'], function (exports, _qunit, _appTestTestsHelpersModuleForAcceptance) {
+
+  (0, _appTestTestsHelpersModuleForAcceptance['default'])('Acceptance | show searchinput tooltip on hover');
+
+  (0, _qunit.test)('Hovering over the question-mark icon', function (assert) {
+    visit('/search');
+    var searchRouteName = 'search',
+        message = 'Enter a partial or complete name';
+
+    andThen(function () {
+      assert.equal(currentRouteName(), searchRouteName, 'search page reached');
+      assert.equal(find('.full-name .info-icon').length, 1, 'name info icon exist');
+    });
+
+    andThen(function () {
+      var ic = find('.full-name .info-icon');
+      ic.mouseenter();
+    });
+
+    andThen(function () {
+      // debugger;
+      var messageBox = find('.full-name .popover-content');
+      assert.equal(messageBox.length, 1, 'popover box is present');
+      assert.equal(messageBox.text().trim().search(message), 0, 'helpful message is shown');
+    });
+
+    andThen(function () {
+      var ic = find('.full-name .info-icon');
+      ic.mouseleave();
+    });
+
+    andThen(function () {
+      var messageBox = find('.full-name .popover-content');
+      assert.equal(messageBox.length, 0, 'popover box is removed');
+    });
+  });
+});
+define('app-test/tests/acceptance/show-searchinput-tooltip-on-hover-test.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - acceptance/show-searchinput-tooltip-on-hover-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'acceptance/show-searchinput-tooltip-on-hover-test.js should pass jshint.');
+  });
+});
+define('app-test/tests/app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass jshint.');
+  });
+});
+define('app-test/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('app-test/tests/helpers/destroy-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers/destroy-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('app-test/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'app-test/tests/helpers/start-app', 'app-test/tests/helpers/destroy-app'], function (exports, _qunit, _appTestTestsHelpersStartApp, _appTestTestsHelpersDestroyApp) {
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _appTestTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        (0, _appTestTestsHelpersDestroyApp['default'])(this.application);
+
+        if (options.afterEach) {
+          options.afterEach.apply(this, arguments);
+        }
+      }
+    });
+  };
+});
+define('app-test/tests/helpers/module-for-acceptance.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers/module-for-acceptance.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('app-test/tests/helpers/resolver', ['exports', 'ember/resolver', 'app-test/config/environment'], function (exports, _emberResolver, _appTestConfigEnvironment) {
+
+  var resolver = _emberResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _appTestConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _appTestConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('app-test/tests/helpers/resolver.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers/resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('app-test/tests/helpers/start-app', ['exports', 'ember', 'app-test/app', 'app-test/config/environment'], function (exports, _ember, _appTestApp, _appTestConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    var attributes = _ember['default'].merge({}, _appTestConfigEnvironment['default'].APP);
+    attributes = _ember['default'].merge(attributes, attrs); // use defaults, but you can override;
+
+    _ember['default'].run(function () {
+      application = _appTestApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('app-test/tests/helpers/start-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - helpers/start-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('app-test/tests/router.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - router.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass jshint.');
+  });
+});
+define('app-test/tests/routes/search.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - routes/search.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/search.js should pass jshint.');
+  });
+});
+define('app-test/tests/test-helper', ['exports', 'app-test/tests/helpers/resolver', 'ember-qunit'], function (exports, _appTestTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_appTestTestsHelpersResolver['default']);
+});
+define('app-test/tests/test-helper.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - test-helper.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+define('app-test/tests/unit/routes/search-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:search', 'Unit | Route | search', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('app-test/tests/unit/routes/search-test.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - unit/routes/search-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/search-test.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('app-test/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
