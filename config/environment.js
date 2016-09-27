c1668348de881290baeb7a84b67683cb9b8640cb
Marcellin Nshimiyimana <mars@fusemachines.com>
2016-09-27 15:45:13 -0400

    use / baseurl

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/tutorial-ember-cli-1.13.14/';
+    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
